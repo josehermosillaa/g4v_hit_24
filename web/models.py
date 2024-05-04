8793053bc84d17d7d05bd182c3567007b17ab027
josehermosillaa <jose.hermosillaa@usach.cl>
2024-05-04 12:14:58 -0400

    comienzo hito3 explicacion

--- web/models.py
+++ web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+#models -> tabla en la BD
+
+class Flan(models.Model):
+    flan_uuid = models.UUIDField()
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    image_url = models.URLField()
+    slug = models.SlugField()
+    is_private = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.name}"
--- web/views.py
+++ web/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from .models import Flan
 
 def indice(request):
-    return render(request, 'index.html', {})
+    #el contexto, es el diccionario donde se envian los datos
+    # flanes = Flan.objects.all() #SELECT * FROM FLAN
+    public_flans = Flan.objects.filter(is_private=False)
+    context = {
+        'public_flans':public_flans
+    }
+    return render(request, 'index.html', context)
+
 
 def acerca(request):
     return render(request, 'about.html', {})
 
 def bienvenido(request):
-    return render(request, 'welcome.html', {})
+    private_flans = Flan.objects.filter(is_private=True)
+    context = {
+        'private_flans': private_flans
+    }
+    return render(request, 'welcome.html', context)
